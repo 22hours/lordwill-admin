ba4ddab0df6785be53f0a4a9e246ad2a247d2770
김효빈 <52522662+hbin12212@users.noreply.github.com>
2022-01-19 13:05:14 +0900

    [FEAT] : 로그인 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React, { useContext, useMemo, useState } from "react";
 import "./App.css";
 import { Button } from "antd";
 import PageLayout from "./components/PageLayout";
+// import crypto from "crypto";
+import { API_CALL } from "./api/api";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MemberPage from "./pages/MemberPage";
 import BookPage from "./pages/BookPage";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import hashSecret from "./secure/hashSecret";
 
 // TYPES
 type Auth = {
@@ -22,7 +25,26 @@ export const AuthDispatchContext = React.createContext<DispatchValues | null>(nu
 
 const App = () => {
     const [auth, setAuth] = useState<any>();
-    const login = (id: string, pw: string) => {
+
+    // const hashPassword = (pw: string) => {
+    //     const secret = hashSecret;
+    //     const hashed = crypto.createHmac("sha256", secret).update(pw).digest("hex");
+    //     return hashed;
+    // };
+
+    const login = async (id: string, pw: string) => {
+        // const res = await API_CALL("POST", "LOGIN", undefined, {
+        //     email: id,
+        //     password: pw,
+        // });
+        // if (res?.result === "SUCCESS") {
+        //     console.log(res);
+        //     // localStorage.setItem("user", JSON.stringify(nowUser));
+        // } else {
+        //     if (res) {
+        //         alert(res.msg);
+        //     }
+        // }
         setAuth({
             id: id,
         });
@@ -30,9 +52,11 @@ const App = () => {
     const logout = () => {
         setAuth(null);
     };
+
     const memoizedDispatches = useMemo(() => {
         return { login, logout };
     }, []);
+
     return (
         <BrowserRouter>
             <AuthContext.Provider value={auth}>
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -22,32 +22,40 @@ type Props = {};
 // COMPONENT
 
 const LoginPage = (props: Props) => {
-  const auth = useContext(AuthContext);
-  const authDispatch = useContext(AuthDispatchContext);
+    const auth = useContext(AuthContext);
+    const authDispatch = useContext(AuthDispatchContext);
 
-  const id = useInput();
-  const pw = useInput();
-  const onSubmit = () => {
-    if (authDispatch) {
-      authDispatch.login(id.value, pw.value);
+    const id = useInput();
+    const pw = useInput();
+
+    const onSubmit = () => {
+        console.log(authDispatch);
+        if (authDispatch) {
+            if (id !== null || pw !== null) {
+                authDispatch.login(id.value, pw.value);
+            } else {
+            }
+        } else {
+            alert("다시 시도해주세요");
+            return;
+        }
+    };
+
+    if (auth) {
+        return <Navigate to={"/member"} replace />;
     }
-  };
-
-  if (auth) {
-    return <Navigate to={"/member"} replace />;
-  }
-  return (
-    <div className={style.container}>
-      <div className={style.login_wrapper}>
-        <h2>LOGIN</h2>
-        <Input {...id} placeholder="EMAIL" maxLength={100} />
-        <Input.Password {...pw} placeholder="PW" maxLength={15} showCount />
-        <Button type="primary" onClick={onSubmit}>
-          로그인
-        </Button>
-      </div>
-    </div>
-  );
+    return (
+        <div className={style.container}>
+            <div className={style.login_wrapper}>
+                <h2>LOGIN</h2>
+                <Input {...id} placeholder="EMAIL" maxLength={100} />
+                <Input.Password {...pw} placeholder="PW" maxLength={15} showCount />
+                <Button type="primary" onClick={onSubmit}>
+                    로그인
+                </Button>
+            </div>
+        </div>
+    );
 };
 
 export default LoginPage;
